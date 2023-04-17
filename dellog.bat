d4e60b4015e92462ab4faf6a3d3db8f8ac94d9a6
a2078233959 <2078233959@qq.com>
2023-04-18 02:31:33 +0800

    Initial commit

---
+++ dellog.bat
@@ -0,0 +1,16 @@
+rd /s /q D:\jeepay9org\jeepay-agent\log
+
+rd /s /q D:\jeepay9org\jeepay-manage\log
+
+rd /s /q D:\jeepay9org\jeepay-merchant\log
+
+rd /s /q D:\jeepay9org\jeepay-pay\log
+
+rd /s /q D:\jeepay9org\jeepay-service\log
+
+rd /s /q D:\jeepay9org\jeepay-task\log
+
+rd /s /q D:\jeepay9org\jeepay-index\log
+
+del /f /s /q D:\jeepay9org\zookeeper\version-2\*.*
+
